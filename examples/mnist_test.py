46db981bb91ed98528aab58bd49df961333b1e9a
Jonathan Xu <jonathan.xu.personal@gmail.com>
2025-06-06 03:35:57 -0400

    clean code

--- examples/mnist_test.py
+++ examples/mnist_test.py
@@ -13,24 +13,16 @@ from ml.models.model import Model
 # load mnist data
 (x_train_raw, y_train_raw), (x_test_raw, y_test_raw) = load_mnist()
 
-# convert to numpy arrays
-x_train = np.array(x_train_raw, dtype=np.float32)
+# convert to numpy arrays and normalize pixel values to [0,1]
+x_train = np.array(x_train_raw, dtype=np.float32) / 255.0
 y_train = np.array(y_train_raw, dtype=np.uint8)
-x_test  = np.array(x_test_raw,  dtype=np.float32)
+x_test  = np.array(x_test_raw,  dtype=np.float32) / 255.0
 y_test  = np.array(y_test_raw,  dtype=np.uint8)
 
-# normalize pixel values to [0,1]
-x_train /= 255.0
-x_test  /= 255.0
-
-# reshape inputs
-x_train = x_train.reshape(-1, 1, 28, 28)
-x_test  = x_test.reshape(-1, 1, 28, 28)
-
-# convert integer labels to one hot
+# one hot encode labels
 num_classes = 10
-y_train = np.eye(num_classes, dtype=np.float32)[y_train]
-y_test  = np.eye(num_classes, dtype=np.float32)[y_test]
+y_train = np.eye(num_classes, dtype=np.uint8)[y_train]
+y_test  = np.eye(num_classes, dtype=np.uint8)[y_test]
 
 model = Model(
     layers=[
